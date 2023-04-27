f202663680de8a3bbe253ebfc7ada2ee2fd8cc31
璃月听书人 <15810728+ddki@users.noreply.github.com>
2023-04-28 00:38:54 +0800

    feat(custom): init

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,54 @@
     windows_subsystem = "windows"
 )]
 
-mod app_config;
-use crate::app_config::menu;
+mod app;
+use tauri::Manager;
 
+use crate::app::menu;
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    message: String,
+}
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .menu(menu::build_menu())
         .on_menu_event(menu::handle_menu_event)
+        .setup(|app| {
+            // listen to the `event-name` (emitted on any window)
+            let id = app.listen_global("listen_global", |event| {
+                println!("got click with payload {:?}", event.payload());
+            });
+            // unlisten to the event using the `id` returned on the `listen_global` function
+            // a `once_global` API is also exposed on the `App` struct
+            app.unlisten(id);
+
+            // emit the `event-name` event to all webview windows on the frontend
+            app.emit_all(
+                "click_emit",
+                Payload {
+                    message: "Tauri is awesome!".into(),
+                },
+            )
+            .unwrap();
+            let external = app.get_window("external").unwrap();
+            external.set_cursor_visible(true).unwrap();
+            let _docs_window = tauri::WindowBuilder::new(
+                app,
+                "github", /* the unique window label */
+                tauri::WindowUrl::External("https://github.com/".parse().unwrap()),
+            )
+            .build()
+            .expect("failed to build window");
+            let _local_window = tauri::WindowBuilder::new(
+                app,
+                "local-new",
+                tauri::WindowUrl::App("index.html".into()),
+            )
+            .build()?;
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,24 @@
 		调用rust
 	</button>
 	<p>rust返回信息：{{ message }}</p>
+
+	<h2>event</h2>
+	<div class="grid gap-2 grid-cols-2 p-2">
+		<button class="border-solid border-[2px] border-black" @click="globalEvent">全局事件</button>
+		<button class="border-solid border-[2px] border-black" @click="listenGlobalEvent">监听全局事件</button>
+		<button class="border-solid border-[2px] border-black" @click="appWindowEvent">
+			appWindow-窗口事件（当前窗口）
+		</button>
+		<button class="border-solid border-[2px] border-black" @click="webViewWindowEvent">
+			WebviewWindow-窗口事件（新窗口）
+		</button>
+	</div>
 </template>
 
 <script setup lang="ts">
 import { invoke } from '@tauri-apps/api'
+import { emit, listen } from '@tauri-apps/api/event'
+import { appWindow, WebviewWindow } from '@tauri-apps/api/window'
 import { ref } from 'vue'
 
 const message = ref()
@@ -15,5 +29,28 @@ const btnClick = () => {
 	invoke('greet', { name: 'World' })
 		// `invoke` returns a Promise
 		.then((response: any) => (message.value = response))
+}
+
+const globalEvent = () => {
+	console.log('globalEvent')
+	emit('click_emit', {
+		message: 'Tauri is awesome!'
+	})
+}
+const listenGlobalEvent = async () => {
+	console.log('listenGlobalEvent')
+	await listen('click', (event) => {
+		console.log(event.event, event.payload)
+	})
+}
+
+const appWindowEvent = () => {
+	console.log('appWindowEvent')
+	appWindow.emit('event', { message: 'Tauri is awesome!' })
+}
+const webViewWindowEvent = () => {
+	console.log('webViewWindowEvent')
+	const webview = new WebviewWindow('window')
+	webview.emit('event')
 }
 </script>
