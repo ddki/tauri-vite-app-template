6b7d883ddc6a552c3b3701be15d77ffdb93e3c46
璃月听书人 <Pinnsvin@163.com>
2023-05-24 23:04:35 +0800

    feat:init

--- src-tauri/src/app/menu.rs
+++ src-tauri/src/app/menu.rs
@@ -1,8 +1,9 @@
 use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, WindowMenuEvent};
+use webbrowser;
 
 pub fn build_menu() -> Menu {
     let setting = CustomMenuItem::new("setting", "设置");
-    let about = about_menu();
+    let about: Submenu = about_menu();
 
     Menu::new()
         .add_item(setting)
@@ -12,6 +13,28 @@ pub fn build_menu() -> Menu {
 
 pub fn handle_menu_event(event: WindowMenuEvent) {
     match event.menu_item_id() {
+        "setting" => {
+            println!("setting...");
+        }
+        "about" => {
+            println!("about...");
+        }
+        "wiki" => {
+            println!("wiki...");
+        }
+        "issues" => {
+            if !webbrowser::open("https://github.com/ddki/tauri-vite-app-demo/issues").is_ok() {
+                print!("打开失败")
+            }
+        }
+        "github" => {
+            if !webbrowser::open("https://github.com/ddki/tauri-vite-app-demo").is_ok() {
+                print!("打开失败")
+            }
+        }
+        "check_update" => {
+            println!("check_update...");
+        }
         "quit" => {
             std::process::exit(0);
         }
@@ -30,6 +53,6 @@ fn about_menu() -> Submenu {
             .add_item(CustomMenuItem::new("wiki", "文档"))
             .add_item(CustomMenuItem::new("issues", "Issues"))
             .add_item(CustomMenuItem::new("github", "Github"))
-            .add_item(CustomMenuItem::new("checkUpdate", "检查更新")),
+            .add_item(CustomMenuItem::new("check_update", "检查更新")),
     )
 }
