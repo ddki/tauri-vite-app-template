b9693afc48bdbacfa894f6f8facd8fb9e345ff73
璃月听书人 <Pinnsvin@163.com>
2023-06-01 16:50:22 +0800

    feat:add updater

---
+++ UPDATE_LOG.md
@@ -0,0 +1,10 @@
+# UPDATE LOG
+
+## v0.1.1
+
+- feat: xx
+- fix: xx
+
+## v0.1.0
+
+test
---
+++ scripts/utils.ts
@@ -0,0 +1,18 @@
+import path from 'path'
+import minimist from 'minimist'
+import { createRequire } from 'module'
+
+export const require = createRequire(import.meta.url)
+
+export const $argv = () => minimist(process.argv.slice(2))
+
+// file
+export const ROOT_PATH = process.cwd()
+export const PACKAGE_PATH = path.join(ROOT_PATH, 'package.json')
+export const UPDATE_LOG_PATH = path.join(ROOT_PATH, 'UPDATE_LOG.md')
+export const TAURI_CONF_PATH = path.join(ROOT_PATH, 'src-tauri', 'tauri.conf.json')
+export const UPDATER_JSON_PATH = path.join(ROOT_PATH, 'updater', 'install.json')
+export const RELEASE_YML_PATH = path.join(ROOT_PATH, '.github', 'workflows', 'release.yml')
+export const packageJSON = (): Record<string, any> => require(PACKAGE_PATH)
+export const tauriConfJSON = (): Record<string, any> => require(TAURI_CONF_PATH)
+export const relativePath = (p: string): string => `${p.split(ROOT_PATH)?.[1].substring(1)}` || ''
