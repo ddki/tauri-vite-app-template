de0d5237ac21a96747fe00a8379b8a400acf18ea
璃月听书人 <Pinnsvin@163.com>
2023-06-09 16:14:06 +0800

    ci: :ferris_wheel: modify release-it config

--- .release-it.cjs
+++ .release-it.cjs
@@ -1,19 +1,61 @@
 module.exports = {
+	npm: {
+		allowSameVersion: true
+	},
+	hooks: {
+		'after:bump': 'pnpm format'
+	},
 	plugins: {
 		'@release-it/conventional-changelog': {
 			writerOpts: {},
-			infile: 'CHANGELOG.md',
+			infile: 'CHANGE_LOG.md',
+			header: '# 📄 更新日志',
 			preset: {
 				name: 'angular',
-				header: '# 📄 更新日志',
 				types: [
 					{
 						type: 'feat',
-						section: '✨ 新增功能'
+						section: '✨ 新增功能 | A new feature'
 					},
 					{
 						type: 'fix',
-						section: '🐛 修复缺陷'
+						section: '🐛 修复缺陷 | A bug fix'
+					},
+					{
+						type: 'docs',
+						section: '📝 文档更新 | Documentation only changes'
+					},
+					{
+						type: 'style',
+						section: '💄 代码格式 | Changes that do not affect the meaning of the code'
+					},
+					{
+						type: 'refactor',
+						section: '♻️ 代码重构 | A code change that neither fixes a bug nor adds a feature'
+					},
+					{
+						type: 'perf',
+						section: '⚡️ 性能提升 | A code change that improves performance'
+					},
+					{
+						type: 'test',
+						section: '✅ 测试相关 | Adding missing tests or correcting existing tests'
+					},
+					{
+						type: 'build',
+						section: ' 📦️ 构建相关 | Changes that affect the build system or external dependencies'
+					},
+					{
+						type: 'ci',
+						section: '🎡 持续集成 | Changes to our CI configuration files and scripts'
+					},
+					{
+						type: 'revert',
+						section: '⏪️ 回退代码 | Revert to a commit'
+					},
+					{
+						type: 'chore',
+						section: '🔨 其他修改 | Other changes that do not modify src or test files'
 					}
 				]
 			}
